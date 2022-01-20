ca6ef3b815668ef620db463076e63c8704fd443d
Alistair Begg <begg.a@etoncollege.org.uk>
2022-01-20 11:49:00 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Begg, Alistair (AMM) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
